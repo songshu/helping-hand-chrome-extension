b950e3f43dbfe40ef2fba015b96011ded170e80c
songshu <jd.songshu@gmail.com>
2017-03-29 02:21:21 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Helping Hand Chrome extension (skeleton).
+
+A base extension for starting out projects.
+
+![alt text](docs/demo_scrshot.png "Sample screen shot")
---
+++ app.js
@@ -0,0 +1,4 @@
+/* Add js - i.e.
+$('body').append('<h1>hello, testing 123...</h1>');
+
+will inject html into the end of the body section of the page you are visiting. */
